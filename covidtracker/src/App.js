d194b599da57e0a302da7b09947761924336befa
priyasmini <priyasmini.sahoo@aptusdatalabs.com>
2020-07-21 00:07:27 +0530

    npx create-react-app and cleanup project

---
+++ covidtracker/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Header */}
+      {/* Title + select input dropdown field */}
+
+      {/** InfoBoxs */}
+      {/** InfoBoxs */}
+      {/** InfoBoxs */}
+
+      {/** Table */}
+      {/** Graph */}
+
+      {/** Map */}
+
+    </div>
+  );
+}
+
+export default App;
